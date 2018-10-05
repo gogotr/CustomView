2d1275c65eb9da99da2f12bfb2c4ddeb8a7cb09c
gogotr <a920609@daum.net>
2018-10-05 12:34:12 +0900

    first commit

--- CustomView/ViewController.swift
+++ CustomView/ViewController.swift
@@ -12,8 +12,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let btn = UIButton(type: UIButtonType.system)
+        btn.frame = CGRect(x: 50, y: 100, width: 150, height: 30)
+        btn.setTitle("test button", for: UIControlState.normal)
+        btn.center = CGPoint(x: self.view.frame.size.width / 2, y: 100)
+        self.view.addSubview(btn)
+        
+        
+        //what is #selector??
+        //@objc으로 선언된 함수를 참조하는 키워드
+        btn.addTarget(self, action: #selector(btnClick(_:)) ,for: .touchUpInside)
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @objc func btnClick(_ sender:Any)
+    {
+        if let btn = sender as? UIButton{
+            btn.setTitle("btn have been cliked", for: UIControlState.normal)
+        }
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
